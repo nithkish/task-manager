465dda84441ff5c71b2f1163408ccb534fee0610
Nithin <nithinkishoreoffcial@gmail.com>
2023-12-04 21:44:48 +0000

    JIRA-00000 Bug fix

---
+++ src/layout/SideBar/menu-items-styles.js
@@ -0,0 +1,28 @@
+import { menuClasses } from "react-pro-sidebar";
+import { themes } from "../../themes";
+import { hexToRgba } from "../../utils/hex-to-rgba";
+
+export const menuItemStyles = (theme) => ({
+  root: {
+    fontSize: "13px",
+    fontWeight: 400,
+  },
+  icon: {
+    color: themes[theme].menu.icon,
+    [`&.${menuClasses.disabled}`]: {
+      color: themes[theme].menu.disabled.color,
+    },
+  },
+  button: {
+    [`&.${menuClasses.disabled}`]: {
+      color: themes[theme].menu.disabled.color,
+    },
+    "&:hover": {
+      backgroundColor: hexToRgba(themes[theme].menu.hover.backgroundColor, 0.7),
+      color: themes[theme].menu.hover.color,
+    },
+  },
+  label: ({ open }) => ({
+    fontWeight: open ? 600 : undefined,
+  }),
+});
