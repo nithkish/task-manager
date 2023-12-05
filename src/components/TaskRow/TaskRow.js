9a1504047cae396212384cfa439aa885a4da5540
Nithin <nithinkishoreoffcial@gmail.com>
2023-12-05 14:06:53 +0000

    JIRA-0000 Bug fix

--- src/components/TaskRow/TaskRow.js
+++ src/components/TaskRow/TaskRow.js
@@ -1,27 +1,29 @@
 import useLocalStorageState from "use-local-storage-state";
 import { Stack } from "@mui/material";
 import AssignmentTurnedInIcon from "@mui/icons-material/AssignmentTurnedIn";
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
-import { HeadlineWrapper, NameText, Text } from "./styled";
+import { HeadlineWrapper, NameText, Text, CustomButton } from "./styled";
 
 export const TaskRow = ({ contract, name, status }) => {
   const [theme] = useLocalStorageState("theme");
 
   const navigate = useNavigate();
-  const navigateToDetails = ()=>{
-    navigate(`./taskdetails/${contract}`)
-  }
+  const navigateToDetails = () => {
+    navigate(`./taskdetails/${contract}`);
+  };
   return (
-    <HeadlineWrapper theme={theme} onClick={navigateToDetails}>
-      <Stack direction="row" gap={1}>
-        <AssignmentTurnedInIcon />
-        <Text>{contract}</Text>
-      </Stack>
-      <Stack direction="row" flex={1}>
-        <NameText>{name}</NameText>
-        <Text>{status}</Text>
-      </Stack>
-    </HeadlineWrapper>
+    <CustomButton theme={theme} onClick={navigateToDetails}>
+      <HeadlineWrapper>
+        <Stack direction="row" gap={1}>
+          <AssignmentTurnedInIcon />
+          <Text>{contract}</Text>
+        </Stack>
+        <Stack direction="row" flex={1} justifyContent='space-between'>
+          <NameText>{name}</NameText>
+          <Text>{status}</Text>
+        </Stack>
+      </HeadlineWrapper>
+    </CustomButton>
   );
 };
--- src/components/TaskRow/styled.js
+++ src/components/TaskRow/styled.js
@@ -1,33 +1,55 @@
-import { Stack, Typography } from '@mui/material'
-import { styled } from '@mui/material/styles'
-import { themes } from '../../themes'
+import { Stack, Typography, Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { themes } from "../../themes";
 
-export const HeadlineWrapper = styled(Stack)(({ theme }) => ({
-    gap: 8,
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: '8px 16px',
-    backgroundColor: themes[theme].taskrow.backgroundColor,
-    borderRadius: '8px',
-    position: 'relative',
-    overflow: 'hidden',
-    justifyContent: 'space-between',
-    cursor:'pointer',
-    ":hover": {
-      backgroundColor:themes[theme].taskrow.hoverBg
-    }
-  }))
-  
-  
-  export const NameText = styled(Typography)(() => ({
-    flex: 1,
-    whiteSpace: 'nowrap',
-    textOverflow: 'ellipsis',
-    overflow: 'hidden',
-  }))
+export const HeadlineWrapper = styled(Stack)(() => ({
+  gap:8,
+  size:"small", 
+  flexDirection: "row",
+  alignItems: "center",
+  padding: "8px 16px",
+  borderRadius: "8px",
+  overflow: "hidden",
+  justifyContent: "space-between",
+  width:'100%'
+}));
 
-  export const Text = styled(Typography)(() => ({
-    whiteSpace: 'nowrap',
-    textOverflow: 'ellipsis',
-    overflow: 'hidden',
-  }))
+export const NameText = styled(Typography)(() => ({
+  whiteSpace: "nowrap",
+  textOverflow: "ellipsis",
+  overflow: "hidden",
+}));
+
+export const Text = styled(Typography)(() => ({
+  whiteSpace: "nowrap",
+  textOverflow: "ellipsis",
+  overflow: "hidden",
+}));
+
+export const CustomButton = styled(Button)(({ theme }) => ({
+  boxShadow: "none",
+  borderRadius:'10px',
+  textTransform: "none",
+  padding: "0",
+  lineHeight: 1.5,
+  color:'inherit',
+  backgroundColor: themes[theme].taskrow.backgroundColor,
+  fontFamily: [
+    "-apple-system",
+    "BlinkMacSystemFont",
+    '"Segoe UI"',
+    "Roboto",
+    '"Helvetica Neue"',
+    "Arial",
+    "sans-serif",
+    '"Apple Color Emoji"',
+    '"Segoe UI Emoji"',
+    '"Segoe UI Symbol"',
+  ].join(","),
+  ":hover": {
+    backgroundColor: themes[theme].taskrow.hoverBg,
+  },
+  "&:focus": {
+    boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)",
+  },
+}));
